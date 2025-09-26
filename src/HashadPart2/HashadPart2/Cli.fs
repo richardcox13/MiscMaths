a95f582756d7938f944840ee7f4c0f845a3a127d
Richard Cox <richard@rjcox.co.uk>
2025-09-26 17:05:26 +0100

    HashadPt2: start to build CLI: root and two subcommands

---
+++ src/HashadPart2/HashadPart2/Cli.fs
@@ -0,0 +1,21 @@
+module HashadPart2.Cli
+
+open System.CommandLine
+
+
+let checkCommand() =
+    let checkCommand = Command("check", "Check one or more ranges of numbers are Hashad numbers in given bases")
+    checkCommand
+
+let scanCommand() =
+    let scanCommand = Command("scan", "Scan for Hashad numbers in given bases")
+    scanCommand
+
+let buildCli () =
+    let rootCommand
+         = RootCommand("Hashad number checker (part 2): check bases")
+        |> fun cmd ->
+            cmd.Subcommands.Add(checkCommand())
+            cmd.Subcommands.Add(scanCommand())
+            cmd
+    rootCommand
