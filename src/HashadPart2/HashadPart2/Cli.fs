a13f989affd0a0d090ec0c2c4be64cbc5308e518
Richard Cox <richard@rjcox.co.uk>
2025-09-27 12:00:04 +0100

    HashadPt2: custom parsing to handle single or ranges of numbers

--- src/HashadPart2/HashadPart2/Cli.fs
+++ src/HashadPart2/HashadPart2/Cli.fs
@@ -1,13 +1,38 @@
 module HashadPart2.Cli
 
 open System.CommandLine
+open HashadPart2.NumberRange
 
+let checkRangeArgument =
+    Argument<NumberRange[]>("checkRange")
+        |> fun arg -> (
+            arg.Description <- "One or more numbers or ranges of numbers to check (ranges form start..end (inclusive))"
+            arg.Arity <- ArgumentArity.OneOrMore
+            arg.DefaultValueFactory <- null
+            arg.CustomParser <- (fun argRes -> 
+                let parsed
+                     = argRes.Tokens
+                    |> Seq.map (fun t -> NumberRange.Parse t.Value)
+                    |> Seq.toArray
+                if Seq.exists (fun (r: Result<NumberRange,string>) -> r.IsError) parsed then
+                    for err in (parsed |> Seq.choose (function | Result.Error e -> Some e | _ -> None)) do
+                        argRes.AddError(err)
+                    null
+                else
+                    parsed
+                        |> Seq.map (function | Result.Ok v -> v | _ -> failwith "unreachable")
+                        |> Seq.toArray
+            )
+            arg
+        )
 
 let checkCommand =
     Command("check", "Check one or more ranges of numbers are Hashad numbers in given bases")
         |> fun cmd ->(
+                cmd.Add(checkRangeArgument) 
                 cmd.SetAction(fun ctx ->
-                    printfn "Check command invoked"
+                    let ranges = ctx.GetValue(checkRangeArgument)
+                    printfn "Check command invoked for %s" (ranges |> Seq.map (fun s -> s.ToString()) |> String.concat ", ")
                     0)
                 cmd)
 
