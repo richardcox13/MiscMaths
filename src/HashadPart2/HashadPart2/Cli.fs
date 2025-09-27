e443dd17aa9127d884e566239fcf99f5ce5cb5a4
Richard Cox <richard@rjcox.co.uk>
2025-09-27 10:50:58 +0100

    HashadPt2: buikders of sub-commands don't need to be functions, use overloaded helper to compose

--- src/HashadPart2/HashadPart2/Cli.fs
+++ src/HashadPart2/HashadPart2/Cli.fs
@@ -3,11 +3,11 @@ module HashadPart2.Cli
 open System.CommandLine
 
 
-let checkCommand() =
+let checkCommand =
     let checkCommand = Command("check", "Check one or more ranges of numbers are Hashad numbers in given bases")
     checkCommand
 
-let scanCommand() =
+let scanCommand =
     let scanCommand = Command("scan", "Scan for Hashad numbers in given bases")
     scanCommand
 
@@ -15,7 +15,7 @@ let buildCli () =
     let rootCommand
          = RootCommand("Hashad number checker (part 2): check bases")
         |> fun cmd ->
-            cmd.Add(checkCommand())
-            cmd.Add(scanCommand())
+            cmd.Add(checkCommand)
+            cmd.Add(scanCommand)
             cmd
     rootCommand
